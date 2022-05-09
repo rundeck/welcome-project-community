5e8ca89f8a5eb3b961575e1ade29df60621c213f
Forrest Evans <fdevans@gmail.com>
2022-05-09 09:59:37 -0700

    4.1.0 Updates
    This includes security enhancements to reduce use of static keys.

--- client/scripts/init.sh
+++ client/scripts/init.sh
@@ -1,5 +1,6 @@
 echo "Init Script Starting"
 ## Place Init Script Code here as needed.  This is run as part of DockerFile steps for Client node.
+ssh-keygen -q -t rsa -N '' -f /rundeck-cli/data/keys/id_rsa
 
 # load project
 ./bin/cli load --rundeck_url $RUNDECK_URL --config_file "$CONFIG_FILE" --path /rundeck-cli
---
+++ node-demo/wait_for_key.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+progress_tic() { if [[ -t 1 ]]; then printf -- "%s" "$@"; else printf -- "%s\n" "$@" ;  fi ; }
+
+echo "Running as user: $(whoami)"
+
+INTERVAL=5
+MAXTRY=10
+attempts=0
+
+while (( attempts <= MAXTRY ))
+do
+    if ! test -f "$KEY_FILE"
+    then  progress_tic "."; # output a progress string.
+    else  break; # file exists
+    fi
+    (( attempts += 1 ))  ; # increment attempts attemptser.
+    (( attempts == MAXTRY )) && {
+        echo "FAIL: Reached max try file exists: $KEY_FILE. Exiting."
+        exit 1
+    }
+    sleep "$INTERVAL"; # wait before trying again.
+done
+
+echo "OK: file exists: $(ls -l "$KEY_FILE")"
