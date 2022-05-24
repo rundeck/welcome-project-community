d27eeb2730463467c03dbe9a55868cbf8fb5b5f6
Forrest Evans <fdevans@gmail.com>
2022-05-24 08:25:39 -0700

    Fixups for new version 4.2.1

--- client/scripts/init.sh
+++ client/scripts/init.sh
@@ -2,6 +2,14 @@ echo "Init Script Starting"
 ## Place Init Script Code here as needed.  This is run as part of DockerFile steps for Client node.
 ssh-keygen -q -t rsa -N '' -f /rundeck-cli/data/keys/id_rsa
 
+cat $CONFIG_FILE
+
+chmod +x bin/cli
+
+echo "Data Dir"
+ls -last /rundeck-cli/data
+echo "-----"
+
 # load project
 ./bin/cli load --rundeck_url $RUNDECK_URL --config_file "$CONFIG_FILE" --path /rundeck-cli
 ./bin/cli addUsers --rundeck_url $RUNDECK_URL --config_file "$CONFIG_FILE" --path /rundeck-cli
